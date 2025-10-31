c7afcbb9dc364c31c1c8e6d9214771f7fe95979d
Vovsanka <drobitkovolodya@gmail.com>
2025-10-31 18:29:44 +0100

    set up the cmake project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+
+int Image[3][3] = {
+    {50, 10, 80},
+    {50, 10, 80},
+    {50, 10, 80}
+};
+
+
+int main() {
+    std::cout << Image[1][1] << std::endl;
+    return 0;
+}
