a91aeb28350eef1d8fea2fccf0fa47606907f992
Vovsanka <drobitkovolodya@gmail.com>
2025-11-02 00:06:41 +0100

    identify edge and corner pixels (example table.png)

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,20 @@
 
 int main() {
     
-    // 3x3 grayscale image
-    cv::Mat image = (cv::Mat_<uchar>(5,5) <<
-        50, 50, 50, 10, 80,
-        50, 50, 10, 80, 80,
-        50, 50, 10, 80, 80,
-        50, 50, 10, 80, 80,
-        50, 50, 10, 80, 80
-    );
+    // Configuration start
+    const int SOBEL_KSIZE = 3; // kernel size of the sobel filter
+    double SIGMA = 1.0; // standard deviation of the Gaussian
+    cv::Size NEIGHBORHOOD = cv::Size(3, 3); // neighborhood for the Gausssian smoothing
+    double THRESHOLD_EIGENVALUE = 0.01; // *max_eigenvalue (determine large/small)
+    // Configuration end
 
-    cv::imshow("Harris Corners", image);
-    cv::waitKey(0);
+    // Load a grayscale image
+    cv::Mat image = cv::imread("../images/table.png", cv::IMREAD_GRAYSCALE);
 
-    // Compute image gradients (Sobel)
+    // Compute image gradients (Sobel filter)
     cv::Mat Ix, Iy;
-    cv::Sobel(image, Ix, CV_64F, 1, 0, 3); // dI/dx
-    cv::Sobel(image, Iy, CV_64F, 0, 1, 3); // dI/dy
-
-    // std::cout << Ix << std::endl;
-    // std::cout << Iy << std::endl;
+    cv::Sobel(image, Ix, CV_64F, 1, 0, SOBEL_KSIZE); // dI/dx
+    cv::Sobel(image, Iy, CV_64F, 0, 1, SOBEL_KSIZE); // dI/dy
     
     // Compute structure tensor components
     cv::Mat Ix2 = Ix.mul(Ix);     // Ix^2
@@ -34,43 +29,63 @@ int main() {
     
     // 🔹 Apply Gaussian smoothing to each product
     cv::Mat Jxx, Jyy, Jxy;
-    double sigma = 1.0; // standard deviation of the Gaussian
 
-    cv::GaussianBlur(Ix2, Jxx, cv::Size(3,3), sigma);
-    cv::GaussianBlur(Iy2, Jyy, cv::Size(3,3), sigma);
-    cv::GaussianBlur(Ixy, Jxy, cv::Size(3,3), sigma);
+    cv::GaussianBlur(Ix2, Jxx, NEIGHBORHOOD, SIGMA);
+    cv::GaussianBlur(Iy2, Jyy, NEIGHBORHOOD, SIGMA);
+    cv::GaussianBlur(Ixy, Jxy, NEIGHBORHOOD, SIGMA);
 
-    // build the structure tensor (for an image pixel)
-    cv::Mat J = (cv::Mat_<double>(2, 2) << 
-        Jxx.at<double>(2,2), Jxy.at<double>(2,2),
-        Jxy.at<double>(2,2), Jyy.at<double>(2,2)
-    );
-
-    std::cout << "\nStructure Tensor (middle pixel):\n";
-    std::cout << J << std::endl;
-
-    // compute the eigenvalues (and eigenvectors)
-    cv::Mat eigenvalues, eigenvectors;
-    cv::eigen(J, eigenvalues, eigenvectors);
-    double l1 = eigenvalues.at<double>(0);
-    double l2 = eigenvalues.at<double>(1);
-    if (l1 < l2) std::swap(l1,l2);
-
-    std::cout << "\nEigenvalues: " << std::endl;
-    std::cout << l1 << " " << l2 << std::endl;
-
-    // classify flatness, edge, corner
-    double treshold = 1000;
-    if (l1 > treshold) {
-        std::cout << "Likely an edge or a corner" << std::endl;
-        if (l2 > treshold) {
-            std::cout << "Corner rather" << std::endl;
-        } else {
-            std::cout << "Edge rather" << std::endl;
+    // build the structure tensors for each pixel and compute the eigenvalues
+    cv::Mat L1(image.size(), CV_64F);
+    cv::Mat L2(image.size(), CV_64F);
+    for (int y = 0; y < Jxx.rows; y++) {
+        for (int x = 0; x < Jxx.cols; x++) {
+            // build the structure tensor for the pixel
+            cv::Mat J = (cv::Mat_<double>(2, 2) <<
+                Jxx.at<double>(y, x), Jxy.at<double>(y, x),
+                Jxy.at<double>(y, x), Jyy.at<double>(y, x)
+            );
+            // compute eigenvalues and eigenvectors
+            cv::Mat eigenvalues, eigenvectors;
+            cv::eigen(J, eigenvalues, eigenvectors);
+            // Store in lambda maps
+            double l1 = eigenvalues.at<double>(0);
+            double l2 = eigenvalues.at<double>(1);
+            if (l1 < l2) std::swap(l1, l2);
+            L1.at<double>(y, x) = l1;
+            L2.at<double>(y, x) = l2;
         }
-    } else {
-        std::cout << "Likely a flat region" << std::endl;
     }
+
+    // classify edges and corners
+    double maxL1, minL1;
+    cv::minMaxLoc(L1, &minL1, &maxL1);
+    double threshold = THRESHOLD_EIGENVALUE * maxL1;
+    cv::Mat E(image.size(), CV_64F);
+    for (int y = 0; y < Jxx.rows; y++) {
+        for (int x = 0; x < Jxx.cols; x++) {
+            double l1 = L1.at<double>(y, x);
+            double l2 = L2.at<double>(y, x);
+            if (l1 > threshold) { // edge or corner
+                // if (l2 > threshold) { // corner
+                //     E.at<double>(y,x) = 2;
+                // } else {
+                //     E.at<double>(y,x) = 1;
+                // }
+                E.at<double>(y,x) = 1;
+            } else {
+                E.at<double>(y,x) = 0;
+            }
+        }
+    }
+
+
+    cv::Mat eNorm;
+    cv::normalize(E, eNorm, 0, 255, cv::NORM_MINMAX);
+    eNorm.convertTo(eNorm, CV_8U);
+
+    cv::imshow("Table", image);
+    cv::imshow("Lambda1", eNorm);
+    cv::waitKey(0);
     
     return 0;
 }
