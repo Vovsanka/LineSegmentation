a44f415d14be85856ba548e29919301b599f4fc9
Vovsanka <drobitkovolodya@gmail.com>
2025-11-02 19:39:30 +0100

    compute all scores

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,16 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
 
+const double PI = std::acos(-1.0);
 
 // Configuration start
 const int SOBEL_KSIZE = 3; // kernel size of the sobel filter
 const double SIGMA = 1.0; // standard deviation of the Gaussian
 const cv::Size NEIGHBORHOOD = cv::Size(3, 3); // neighborhood for the Gausssian smoothing
 const double THRESHOLD_EIGENVALUE = 0.25; // *mean_eigenvalue1 (determine large/small)
+const int DIRECTIONS = 8;
 // Configuration end
 
 void showImage(const cv::Mat &F) {
@@ -85,6 +88,43 @@ cv::Mat classifyPixels(const cv::Mat &L1, const cv::Mat &L2) {
     return T;
 }
 
+std::vector<cv::Mat> computeAllScores(
+    const cv::Mat &Jxx,
+    const cv::Mat &Jyy,
+    const cv::Mat &Jxy,
+    const cv::Mat &L1,
+    const cv::Mat &L2,
+    const cv::Mat &T
+) {
+    std::vector<cv::Mat> S;
+    // Compute the score matrix for every direction
+    for (int d = 0; d < DIRECTIONS; d++) {
+        // fix the normal unit vector for the direction
+        double rad = d * (PI/DIRECTIONS);
+        double unitY = std::asin(rad);
+        double unitX = std::acos(rad);
+        // compute the score matrix for the direction
+        cv::Mat SCORE(Jxx.size(), CV_64F);
+        for (int y = 0; y < Jxx.rows; y++) {
+            for (int x = 0; x < Jxx.cols; x++) {
+                double jxx = Jxx.at<double>(y, x);
+                double jyy = Jyy.at<double>(y, x);
+                double jxy = Jxy.at<double>(y, x);
+                double l1 = L1.at<double>(y, x);
+                double l2 = L2.at<double>(y, x);
+                double e = T.at<double>(y, x);
+                double score = 0;
+                if (e > 0) {
+                    double pr = unitY*unitY*jxx + 2*unitY*unitX*jxy + unitX*unitX*jyy;
+                    score = (pr - l2) / (l1 - l2);
+                }
+                SCORE.at<double>(y, x) = score;
+            }
+        }
+        S.emplace_back(SCORE);
+    }
+    return S;
+}
 
 
 int main() {
@@ -103,34 +143,14 @@ int main() {
 
     cv::Mat T = classifyPixels(L1, L2);
 
-    showImage(T);
+    // showImage(T);
 
-    // Direction projection and score example
-    // std::pair<int, int> r;
-    // std::pair<double, double> n = {1.0, 0}; // normal unit vector for a direction
-    // double score = computeScore(Jxx, Jyy, Jxy, L1, T, r, n);
-    // showImage(S);
+    // Compute all scores
+    std::vector<cv::Mat> S = computeAllScores(Jxx, Jyy, Jxy, L1, L2, T);
 
-    // // Direction projection and score example
-    // double yNorm = 1.0, xNorm = 0; // normal unit vector for a direction
-    // cv::Mat S(image.size(), CV_64F);
-    // for (int y = 0; y < image.rows; y++) {
-    //     for (int x = 0; x < image.cols; x++) {
-    //         double jxx = Jxx.at<double>(y, x);
-    //         double jyy = Jyy.at<double>(y, x);
-    //         double jxy = Jxy.at<double>(y, x);
-    //         double l1 = L1.at<double>(y, x);
-    //         double l2 = L2.at<double>(y, x);
-    //         double e = E.at<double>(y, x);
-    //         double score = 0;
-    //         if (e > 0) {
-    //             double pr = yNorm*yNorm*jxx + 2*yNorm*xNorm*jxy + xNorm*xNorm*jyy;
-    //             score = (pr - l2) / (l1 - l2);
-    //         }
-    //         S.at<double>(y, x) = score;
-    //     }
-    // }
-    // showMat(S);
-    
+    for (int d = 0; d < DIRECTIONS; d++) {
+        showImage(S[d]);
+    }
+
     return 0;
 }
