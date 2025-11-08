c779b929ddf497dcd4d45e4296a25e4fd5a442ca
Vovsanka <drobitkovolodya@gmail.com>
2025-11-08 02:50:42 +0100

    gpu score comptutation

--- src/main.cu
+++ src/main.cu
@@ -1,15 +1,14 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/cuda.hpp>
+#include <cuda_runtime.h>
 
 #include <iostream>
-#include <stdexcept> // for standard exception types
 #include <cmath>
 #include <vector>
 
-const double PI = std::acos(-1.0);
 
 // Configuration start
-const int DIRECTIONS = 30;
+const int DIRECTIONS = 360;
 const double R = 2.5; // radius for the interesting pixels
 const double OFFSET = 500; // pixel offset
 const double CAND_SCORE = 0.9; // candidate score threshold
@@ -29,34 +28,44 @@ void showImage(const cv::Mat &F) {
     cv::waitKey(0);
 }
 
-double computeScore(const cv::Mat &F, int yPixel, int xPixel, double unitNormY, double unitNormX) {
+template <typename T>
+__device__ T max(T a, T b) {
+    return (a > b) ? a : b;
+}
+
+template <typename T>
+__device__ T min(T a, T b) {
+    return (a > b) ? a : b;
+}
+
+__device__ double computeScore(const float* F,
+                               int yPixel, int xPixel,
+                               double unitNormY, double unitNormX,
+                               int width, int height) {
     const int R_LOWER = std::floor(R);
+    int idx = (yPixel * width + xPixel) * 3;
     // consider only the pixels s.t. their pixel center (y, x) is on/in the circle with radius R
     double r1, g1, b1, r2, b2, g2;
     r1 = g1 = b1 = r2 = b2 = g2 = 0;
     double minR, minG, minB;
     minR = minG = minB = 255;
-    for (int y = std::max(0, yPixel - R_LOWER); y <= std::min(F.rows, yPixel + R_LOWER); y++) {
+    for (int y = max(0, yPixel - R_LOWER); y <= min(height, yPixel + R_LOWER); y++) {
         // solve the quadratic inequation for x: (y - yPixel)^2 + (x - xPixel)^2 <= 0
         double D = 4*(R*R - (y - yPixel)*(y - yPixel));
-        int x1 = std::ceil((2*xPixel - sqrt(D)) / 2); // round up to the next integer
-        int x2 = std::floor((2*xPixel + sqrt(D)) / 2); // round down to the next integer
-        for (int x = std::max(0, x1); x <= std::min(F.cols, x2); x++) {
+        int x1 = ceil((2*xPixel - sqrt(D)) / 2); // round up to the next integer
+        int x2 = floor((2*xPixel + sqrt(D)) / 2); // round down to the next integer
+        for (int x = max(0, x1); x <= min(width, x2); x++) {
             int dy = (y - yPixel), dx = (x - xPixel);
-            if (!(dx*dx + dy*dy <= R*R)) {
-                throw "Assertion failed: pixel outside the cirle!!!";
-            }
             double signedDist = dy*unitNormY + dx*unitNormX;
             double dist = std::abs(signedDist);
             // skip the pixels on the line
             if (dist < 0.1) continue;
             // add pixel to the corresponding half-circle
-            const cv::Vec3b pixel = F.at<cv::Vec3b>(y, x);
-            double b = pixel[0], g = pixel[1], r = pixel[2];
-            double w = R - sqrt(dx*dx + dy*dy);
-            minR = std::min(minR, r);
-            minG = std::min(minG, g);
-            minB = std::min(minB, b);
+            double b = F[idx], g = F[idx + 1], r = F[idx + 2];
+            double w = R*R - (dx*dx + dy*dy);
+            minR = min(minR, r);
+            minG = min(minG, g);
+            minB = min(minB, b);
             if (signedDist >= 0) { // the half-circle of the normal vector
                 b1 += w*b;
                 g1 += w*g;
@@ -69,19 +78,19 @@ double computeScore(const cv::Mat &F, int yPixel, int xPixel, double unitNormY, 
         }
     }
     // equalize intensive and non-intensive colors
-    for (int y = std::max(0, yPixel - R_LOWER); y <= std::min(F.rows, yPixel + R_LOWER); y++) {
+    for (int y = max(0, yPixel - R_LOWER); y <= min(height, yPixel + R_LOWER); y++) {
         // solve the quadratic inequation for x: (y - yPixel)^2 + (x - xPixel)^2 <= 0
         double D = 4*(R*R - (y - yPixel)*(y - yPixel));
         int x1 = std::ceil((2*xPixel - sqrt(D)) / 2); // round up to the next integer
         int x2 = std::floor((2*xPixel + sqrt(D)) / 2); // round down to the next integer
-        for (int x = std::max(0, x1); x <= std::min(F.cols, x2); x++) {
+        for (int x = max(0, x1); x <= min(width, x2); x++) {
             int dy = (y - yPixel), dx = (x - xPixel);
             double signedDist = dy*unitNormY + dx*unitNormX;
             double dist = std::abs(signedDist);
             // skip the pixels on the line
             if (dist < 0.1) continue;
             // add pixel to the corresponding half-circle
-            double w = R - sqrt(dx*dx + dy*dy);
+            double w = R*R - (dx*dx + dy*dy);
             if (signedDist >= 0) {
                 b1 += w*(-minB);
                 g1 += w*(-minG);
@@ -96,32 +105,37 @@ double computeScore(const cv::Mat &F, int yPixel, int xPixel, double unitNormY, 
     // compute the score (add offset to reduce the noise sensitivity)
     double area1 = r1 + g1 + b1 + OFFSET;
     double area2 = r2 + g2 + b2 + OFFSET;
-    double ratio = std::max(area1/area2, area2/area1);
-    return 1.0 - std::pow(1/ratio, 8);
+    double ratio = max(area1/area2, area2/area1);
+    return 1.0 - 1/(ratio*ratio*ratio);
 }
 
-std::tuple<cv::Mat, cv::Mat> computeBestScores(const cv::Mat &F) {
-    cv::Mat S(F.size(), CV_64F), D(F.size(), CV_64F);
+__global__ void bestScoreKernel(const float* F, double* S, double* D,
+                                int width, int height, int directions) {
+    const double PI = std::acos(-1.0);
+
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x >= width || y >= height) return;
+    
     // Compute the score matrix for every direction
-    for (int d = 0; d < DIRECTIONS; d++) {
-        // fix the normal unit vector for the direction
-        double rad = d * (PI/DIRECTIONS);
-        double unitY = std::sin(rad);
-        double unitX = std::cos(rad);
-        // compute the score matrix for the direction
-        for (int y = 0; y < F.rows; y++) {
-            for (int x = 0; x < F.cols; x++) {
-                double score = computeScore(F, y, x, unitY, unitX);
-                double oldScore = -1;
-                if (d > 0) oldScore = S.at<double>(y, x);
-                if (score > oldScore) {
-                    S.at<double>(y, x) = score;
-                    D.at<double>(y, x) = d;
-                }
-            }
+    double bestScore = -1;
+    double bestDir = 0;
+
+    for (int d = 0; d < directions; ++d) {
+        double rad = d * (PI / directions);
+        double unitY = sin(rad);
+        double unitX = cos(rad);
+
+        double score = computeScore(F, x, y, unitX, unitY, width, height);
+        if (score > bestScore) {
+            bestScore = score;
+            bestDir = d;
         }
     }
-    return std::make_tuple(S, D);
+
+    int idx = y * width + x;
+    S[idx] = bestScore;
+    D[idx] = bestDir;
 }
 
 cv::Mat chooseCandiates(const cv::Mat &S) {
@@ -138,22 +152,40 @@ cv::Mat chooseCandiates(const cv::Mat &S) {
 
 int main() {
     
+    // Check the CUDA devices
     int cudaCount = cv::cuda::getCudaEnabledDeviceCount();
     std::cout << "CUDA devices: " << cudaCount << std::endl;
 
-    // // Load an RGB image
-    // cv::Mat F = cv::imread("../images/table.png", cv::IMREAD_COLOR);
+    // Load an RGB image
+    cv::Mat cpuF = cv::imread("../images/table.png", cv::IMREAD_COLOR);
     
-    // // compute the best scores for every pixel
-    // cv::Mat S, D;
-    // std::tie(S, D) = computeBestScores(F);
+    // Upload the image to GPU
+    cv::cuda::GpuMat F;
+    F.upload(cpuF);
+
+    // GPU threads for each pixel
+    dim3 block(16, 16); // 256
+    dim3 grid((F.cols + 15) / 16, (F.rows + 15) / 16); // round up to cover the whole image
+    
+    // compute the best scores for every pixel
+    cv::cuda::GpuMat S(F.size(), CV_64F);
+    cv::cuda::GpuMat D(F.size(), CV_64F);
+    bestScoreKernel<<<grid, block>>>(
+        F.ptr<float>(), S.ptr<double>(), D.ptr<double>(),
+        F.cols, F.rows, DIRECTIONS
+    );
 
     // // choose the candidates
     // cv::Mat C = chooseCandiates(S);
     
-    // showImage(F);
-    // showMatrix(S);
-    // showMatrix(C);
+    // download the images to CPU
+    cv::Mat cpuS;
+    // S.download(cpuS);
+
+    // show the images
+    showImage(cpuF);
+    // showMatrix(cpuS);
+    // showMatrix(cpuC);
 
     return 0;
 }
