25c80db2859e7d98e5aed2b6e583023cccd91eed
Vovsanka <drobitkovolodya@gmail.com>
2025-11-09 01:32:56 +0100

    small fix

--- src/main.cu
+++ src/main.cu
@@ -11,7 +11,7 @@
 const int DIRECTIONS = 360;
 const double R = 1.5; // radius for the interesting pixels
 const double OFFSET = 40; // pixel offset
-const double THICKNESS = 0.2// line thickness
+const double THICKNESS = 0.2; // line thickness
 const double CAND_SCORE = 0.95; // candidate score threshold
 // Configuration end
 
