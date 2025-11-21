67f7c0ba6f9d27352afa298deebff573f50ca556
Vovsanka <drobitkovolodya@gmail.com>
2025-11-21 15:27:54 +0100

    weighted score and nice parameters

--- include/config.hpp
+++ include/config.hpp
@@ -1,12 +1,12 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-const int DIRECTIONS = 180;
-const double R = 5.0; // radius for the circle of interesting pixels
+const int DIRECTIONS = 60;
+const double R = 3.0; // radius for the circle of interesting pixels
 const double STEP = 0.2; // (R/STEP = INT!) step size for the discrete subpixels in the circle
-const double OFFSET = 5; // score offset to reduce the noise sensitivity
+const double OFFSET = 7; // score offset to reduce the noise sensitivity
 const double THICKNESS = 1; // line thickness 
-const double CAND_RATIO = 2.0; // candidate ratio
+const double CAND_RATIO = 1.8; // candidate ratio
 const double THRESHOLD = 0.5; // candidate threshold (the logistic middle)
 
 #endif
--- src/score.cu
+++ src/score.cu
@@ -13,8 +13,8 @@ double computeScore(const uchar* F,
                     double yPixel, double xPixel,
                     int direction, 
                     int width, int height) {
-    int r1, g1, b1, c1, r2, b2, g2, c2;
-    r1 = g1 = b1 = c1 = r2 = b2 = g2 = c2 = 0;
+    double r1, g1, b1, r2, b2, g2;
+    r1 = g1 = b1 = r2 = b2 = g2 = 0;
     int minR, minG, minB;
     minR = minG = minB = 255;
     //
@@ -42,24 +42,43 @@ double computeScore(const uchar* F,
             minR = min(minR, r);
             minG = min(minG, g);
             minB = min(minB, b);
+        }
+    }
+    for (double y = y1; y <= y2; y += STEP) {
+        for (double x = x1; x <= x2; x += STEP) {
+            // check if in the or on the circle
+            double dy = (y - yPixel), dx = (x - xPixel);
+            if (dy*dy + dx*dx > R*R) continue;
             //
+            double signedDist = dy*unitNormY + dx*unitNormX;
+            double dist = abs(signedDist);
+            // skip the pixels on the line
+            if (dist < THICKNESS/2) continue;
+            //
+            double w =  1.0 - sqrt(dx*dx + dy*dy)/R;
+            //
+            thrust::tuple<uchar,uchar,uchar> rgb = getRgbColors(F, y, x, width, height);
+            int r = thrust::get<0>(rgb);
+            int g = thrust::get<1>(rgb);
+            int b = thrust::get<2>(rgb);
+            minR = min(minR, r);
+            minG = min(minG, g);
+            minB = min(minB, b);
+            // equalize intensive and non-intensive colors
             if (signedDist > 0) { // the half-circle of the normal vector
-                c1++;
-                b1 += b;
-                g1 += g;
-                r1 += r;
+                b1 += w*(b - minB + OFFSET);
+                g1 += w*(g - minG + OFFSET);
+                r1 += w*(r - minR + OFFSET);
             } else { // the half-circle opposite to the normal vector
-                c2++;
-                b2 += b;
-                g2 += g;
-                r2 += r;
+                b2 += w*(b - minB + OFFSET);
+                g2 += w*(g - minG + OFFSET);
+                r2 += w*(r - minR + OFFSET);
             }
         }
     }
-    // equalize intensive and non-intensive colors
-    double offset = -(minR + minG + minB) + OFFSET;
-    double area1 = 1.0*(r1 + g1 + b1)/c1 + offset;
-    double area2 = 1.0*(r2 + g2 + b2)/c2 + offset;
+
+    double area1 = r1 + g1 + b1;
+    double area2 = r2 + g2 + b2;
     double ratio = max(area1/area2, area2/area1);
     // compute the score (using the logistic function)
     return 1.0 / (1.0 + exp(-(ratio - CAND_RATIO)));
